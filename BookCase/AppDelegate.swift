cb2d149dca264ca540536df6c38e5091be506bd5
Sathish Kumar S <sathish141994@gmail.com>
2019-09-21 19:09:57 +0530

    Checkin.

--- BookCase/AppDelegate.swift
+++ BookCase/AppDelegate.swift
@@ -12,12 +12,7 @@ import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow.init(frame: UIScreen.main.bounds)
-        window?.rootViewController = MainViewController.init()
-        window?.makeKeyAndVisible()
         return true
     }
 
